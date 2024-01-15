7314b4e4093e0ce642f0dea06aa9ae76f18f2f39
MagicCreator <143210375+MagicCreatorApp@users.noreply.github.com>
2024-01-15 17:21:27 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# What kind of plant is this? 
+
+[Fork FROM](https://github.com/liu-ziting/PetThoughts/)
---
+++ pages/api/upload.js
@@ -0,0 +1,97 @@
+import { createRouter } from 'next-connect'
+import multer from 'multer'
+const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require('@google/generative-ai')
+
+const API_KEY = process.env.GEMINI_API_KEY
+const MODEL_NAME = 'gemini-pro-vision'
+
+const upload = multer({
+    storage: multer.memoryStorage()
+})
+
+const router = createRouter()
+
+router.use(upload.single('image'))
+
+router.all((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+    } else {
+        next()
+    }
+})
+router.post(async (req, res) => {
+    console.log('req.file:', req.file)
+    try {
+        console.log('Starting request...')
+        const genAI = new GoogleGenerativeAI(API_KEY)
+        const model = genAI.getGenerativeModel({ model: MODEL_NAME })
+
+        const generationConfig = {
+            temperature: 1,
+            topK: 32,
+            topP: 1,
+            maxOutputTokens: 8000
+        }
+
+        const safetySettings = [
+            {
+                category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+                threshold: HarmBlockThreshold.BLOCK_NONE
+            },
+            {
+                category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+                threshold: HarmBlockThreshold.BLOCK_NONE
+            },
+            {
+                category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+                threshold: HarmBlockThreshold.BLOCK_NONE
+            },
+            {
+                category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+                threshold: HarmBlockThreshold.BLOCK_NONE
+            }
+        ]
+
+        if (!req.file) {
+            console.log('No file uploaded.')
+            return res.status(400).json({ error: 'No file uploaded.' })
+        }
+
+        const parts = [
+            {
+                text: '你是一位经验丰富的植物识别专家，根据用户上传的照片，你能准确识别出是什么植物，并提供相关的信息和特征。你可以根据植物的形态、颜色、叶片结构和周围环境来确定植物的种类。识别完成后，请根据植物的特点，用中文提供详细的描述和信息，格式如下：🌿：<这是什么植物><植物的信息和特征>。如果用户上传的图片中没有植物，则返回“图片中没有植物""'
+            },
+            {
+                inlineData: {
+                    mimeType: 'image/jpeg',
+                    data: req.file.buffer.toString('base64')
+                }
+            }
+        ]
+
+        const result = await model.generateContent({
+            contents: [{ role: 'user', parts }],
+            generationConfig,
+            safetySettings
+        })
+
+        if (!result) {
+            return res.status(502).json({ error: 'Bad Gateway' })
+        } else {
+            const responseText = result.response.text()
+            return res.status(200).json({ result: responseText })
+        }
+    } catch (error) {
+        return res.status(500).json({ error: 'Internal Server Error' })
+    }
+})
+
+export const config = {
+    api: {
+        bodyParser: false,
+        externalResolver: true
+    }
+}
+
+export default router.handler()
